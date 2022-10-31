9443ec836e487e2d78bd4a648dfd58222b123fdf
Sajid Ali <91059888+devPirate505@users.noreply.github.com>
2022-10-31 22:41:49 +0500

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,6 @@
+radius = float(input("Write the radius: "))
+from math import pi
+area = pi * radius ** 2
+print("Area of Circle is: " , area)
+circumference = 2 * pi * radius
+print("Circumference of Circle is: " , circumference)
